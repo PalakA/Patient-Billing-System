1de8678649f393b882c8c715fa61c7f449eb2ad2
Palak Agarwal <agarwal.pa@northeastern.edu>
2021-07-13 17:35:22 -0400

    Added Project

---
+++ Care Hospital/src/pbs/Pbs.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbs;
+
+/**
+ *
+ * @author student
+ */
+public class Pbs {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Patient-Billing-System
+
+This project is aimed at developing a patient billing software that is of importance to a hospital. The PBS is a local software system. This system can be used to maintain the location (bed) of each patient either in the ward or the ICU. Information about the patient and the charges to be paid is also stored.
